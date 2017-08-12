35381317b6c9fe9c9048d2ff8ed2df2f9bfd05cb
mgaspari <marcoanthonygaspari@gmail.com>
2017-08-12 13:19:58 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+
+  end
+
+  def match(array)
+    returnArray = []
+    @word = @word.split(//)
+      array.each do |testWord|
+        check = testWord.split(//)
+        if @word.sort == check.sort
+          returnArray << testWord
+        end
+      end
+      returnArray
+  end
+end
